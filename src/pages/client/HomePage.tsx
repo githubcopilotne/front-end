d47db0a2aaf9935584d2eab96eb6b67c305ff022
githubcopilot <maildemothoil@gmail.com>
2026-02-04 09:37:17 +0700

    Done new arrival

--- src/pages/client/HomePage.tsx
+++ src/pages/client/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroBanner from '../../components/home/HeroBanner'
 import BestSeller from '../../components/home/BestSeller'
+import NewArrivals from '../../components/home/NewArrivals'
 
 const HomePage = () => {
   return (
     <div>
       <HeroBanner />
       <BestSeller />
+      <NewArrivals />
     </div>
   )
 }
